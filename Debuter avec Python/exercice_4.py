1dd794dd7cd4892b32c681f3c9ca456fcf688fea
ET <146172190+EthanPalous@users.noreply.github.com>
2024-09-24 14:55:41 +0000

    v1.2.2

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -1,14 +1,16 @@
-c = 2
+c = Cote_du_carre
 
+def carre(c):
+   return c ** 2
+   
 def aire_Carre(c):
     A = c ** 2
     return A
 A = aire_Carre(c)
-
-print(A)
+print(aire_Carre(c))
 
 def perimetre_Carre(c):
     P = 4 * c
     return P
 P = perimetre_Carre(c)
-print(P)
+print(perimetre_Carre(c))
